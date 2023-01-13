e60bb97c490408259d42c1689f97a65dce371eda
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2023-01-12 15:49:29 -1000

    Initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,9 @@
-import adapter from '@sveltejs/adapter-node';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
 	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+		adapter: adapter()
 	}
 };
 
